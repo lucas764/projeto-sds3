beb647690aa5982642594dceadb1c5937cfd67b2
Lucas Nascimento <lucas.salvador888@gmail.com>
2021-05-04 21:56:14 -0300

    static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,34 @@
 import Footer from "components/footer"
 import NavBar from "components/NavBar"
 import DataTable from "components/DataTable"
+import BarChat from "components/BarChat"
+import DonutChart from "components/DonutChart"
 
 function App() {
   return (
     <>
-    <NavBar />
-    <div className="container">
-      <h2 className="text-primary">hello word</h2>
-      <DataTable />
-    </div>
-    <Footer />
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dasbord de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChat />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
+      </div>
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
